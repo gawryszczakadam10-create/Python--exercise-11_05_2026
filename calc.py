542e1db1f50865f73b91f5fac5096bd47b81ece2
xDeeFx <filip.pawlowski2010@gmail.com>
2026-05-16 19:30:07 +0200

    added multiply function

--- calc.py
+++ calc.py
@@ -1,6 +1,10 @@
 def add(a, b):
     return a + b
 
+def multiply(a, b):
+    return a * b
+
 if __name__ == "__main__":
     print("Welcome to the Python Calc!")
     print(f"2 + 3 = {add(2, 3)}")
+    print(f"2 * 5 = {multiply(2, 5)}")
